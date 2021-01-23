e2fc67ea390ad29211f49f31659fdc4094478671
joshgelias <josh.g.elias@gmail.com>
2021-01-23 01:08:36 -0500

    wrote gpio boilerplate

--- hydro_services/src/water_pump.rs
+++ hydro_services/src/water_pump.rs
@@ -1,7 +1,35 @@
-pub fn start_pump () {
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
 
+use rppal::gpio::Gpio;
+
+const WATER_PUMP: u8 = 1;
+
+pub fn start_pump() -> Result<(), Box<dyn Error>> {
+    let mut pin = Gpio::new()?.get(WATER_PUMP)?.into_output();
+    pin.set_high();
+    
+    Ok(())
 }
 
-pub fn stop_pump () {
-    
+pub fn stop_pump() -> Result<(), Box<dyn Error>> {
+    let mut pin = Gpio::new()?.get(WATER_PUMP)?.into_output();
+    pin.set_low();
+
+    Ok(())
+}
+
+pub fn hydrate_plant() -> Result<(), Box<dyn Error>> {
+    let mut pin = Gpio::new()?.get(WATER_PUMP)?.into_output();
+    pin.set_low();
+
+    for _ in 0..5 {
+        pin.set_high();
+        thread::sleep(Duration::from_secs(1));
+        pin.set_low();
+        thread::sleep(Duration::from_secs(2));
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use hydro_services::camera;
+use hydro_services::water_pump;
 
 fn main() {
-    camera::take_picture();
+    //camera::take_picture();
+    water_pump::start_pump().unwrap();
 }
